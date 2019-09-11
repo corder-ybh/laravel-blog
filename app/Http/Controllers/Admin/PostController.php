391aa35f402e7bb59d83619f8b0168e8360ba30a
bhyang <wa@q.com>
2019-09-11 17:36:17 +0800

    更新

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -116,7 +116,7 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(PostCreateRequest $request, $id)
     {
         $post = Post::findOrFail($id);
         $post->fill($request->postFillData());
